78d71f8d80e143a10eace5276048058734555fcd
THOMAS FASSIH <thomasfassih@gmail.com>
2022-12-07 08:05:56 -0600

    initial commit

---
+++ src/main/java/com/babl/BABL/BablApplication.java
@@ -0,0 +1,13 @@
+package com.babl.BABL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BablApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BablApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babl/BABL/BablApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babl.BABL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BablApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
