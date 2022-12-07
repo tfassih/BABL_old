bf17d7032de0787b6c3b13b23b5c7731479315ed
THOMAS FASSIH <thomasfassih@gmail.com>
2022-12-07 08:31:10 -0600

    deps

--- src/main/java/com/babl/BABL/BablApplication.java
+++ src/main/java/com/babl/BABL/BablApplication.java
@@ -1,7 +1,11 @@
 package com.babl.BABL;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BablApplication {
@@ -10,4 +14,15 @@ public class BablApplication {
 		SpringApplication.run(BablApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+			System.out.println("Beans: ");
+			String[] names = ctx.getBeanDefinitionNames();
+			Arrays.sort(names);
+			for (String bName : names) {
+				System.out.println(bName);
+			}
+		};
+	}
 }
---
+++ src/main/java/com/babl/BABL/MainController.java
@@ -0,0 +1,13 @@
+package com.babl.BABL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+}
